ccb4d20c31bcf24241c4210e366d934db215afb4
Michael Josh <michaelikejoshua@gmail.com>
2023-09-28 13:09:30 +0100

    update

--- src/page/dashboard/component/LandingPage.jsx
+++ src/page/dashboard/component/LandingPage.jsx
@@ -3,7 +3,7 @@ import "../style/LandingPage.css";
 import axios from 'axios';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
-import SemicolonLogo from "../../../assets/images/SemicolonLogo.svg";
+import logo from "../../../assets/images/SemicolonLogo.svg";
 import YourRoleImage from "../../../assets/images/YourRoleImage.svg";
 import SeamlessCommunication from "../../../assets/images/SeamlessCommunication.svg";
 import CollaborationOpportunities from "../../../assets/images/CollaborationOpportunities.svg";
@@ -24,6 +24,12 @@ function LandingPage() {
     const [currentImage, setCurrentImage] = useState(0);
     const [submitted, setSubmitted] = useState(false);
     const [allFieldsFilled, setAllFieldsFilled] = useState(false);
+
+    const images = [
+        SemicolonImage1,
+        SemicolonImage2,
+        SemicolonImage3,
+    ];
 
     useEffect(() => {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -109,28 +115,31 @@ function LandingPage() {
         <div className="semicolon-network-landing-page">
             <header className="header">
                 <div className="header-left">
-                    <img src={SemicolonLogo} alt="Semicolon Logo" className="semicolon-logo" width="100" height="100"/>
-                    <h1 className="semicolon-name">semicolon</h1>
+                    <img src={logo} alt="Logo" className="semicolon" width="100" height="60"/>
+                    {/* <h1 className="semicolon-name">semicolon</h1> */}
                 </div>
                 <div className="header-right">
                     <button className="vision-button" onClick={() => scrollToSection('vision-section')}>Vision</button>
                     <button className="why-it-matters-button" onClick={() => scrollToSection('why-it-matters-section')}>Why It Matters</button>
                     <button className="your-role-button" onClick={() => scrollToSection('your-role-section')}>Your Role</button>
-                    <button className="join-the-team-button" onClick={() => scrollToSection('join-the-team-section')}>Join The Team</button>
+                    <button className="join-the-team-button" onClick={() => scrollToSection('join-the-team-section')}>Join the team</button>
                 </div>
             </header>
             <div className="semicolon-community">
-                <h1 className="community-header">Building a vibrant <br/> Community Hub</h1>
-                <p className="community-text">Join us on this journey to build a platform that empowers our <br/> school community, fosters meaningful connections, and <br/> redefines how we engage and collaborate. Together, we will <br/> shape the future of our school community's digital presence.</p>
-                <button className="join-the-team-community-button" onClick={() => scrollToSection('join-the-team-section')}>Join The Team</button>
-                <div className="image-scroll-container">
-                    {[
-                        SemicolonImage1,
-                        SemicolonImage2,
-                        SemicolonImage3
-                    ].map((image, index) => (
-                        <img key={index} src={image} alt={`Semicolon ${index + 1}`} style={{width: '1437px', height: '690px', display: index === currentImage ? 'block' : 'none'}}/>
-                    ))}
+                <div className="hero-section">
+                    <h1 className="community-header">Building a vibrant <br/> Community Hub</h1>
+                    <p className="community-text">
+                        Join us on this journey to build a platform that empowers our community,
+                        foster meaningful connections, and redefines how we engage and collaborate.
+                        Together, we will shape the future of our community's digital presence.
+                    </p>
+                    <p className="community-text content">...built by ancestors and natives, for ancestors and natives</p>
+                    <button className="join-the-team-community-button" onClick={() => scrollToSection('join-the-team-section')}>Join the team</button>
+                </div>
+                <div className="image-scroll-container" >
+                    {/* {images.map((image, index) => (
+                        <img key={index} src={image} alt={`Semicolon ${index + 1}`} style={{width: '100%', display: index === currentImage ? 'block' : 'none'}}/>
+                    ))} */}
                 </div>
             </div>
             <div className="what-we-envision" id="vision-section">
@@ -140,29 +149,29 @@ function LandingPage() {
                         <h3 className="what-we-envision-sub-header"><span style={{ color: "#ff2e2e" }}>Seamless</span> <br/> Communication
                             <img className="what-we-envision-image" src={SeamlessCommunication} alt="Seamless Communication" width="80" height="80"/>
                         </h3>
-                        <p className="what-we-envision-text">Imagine a platform that <br/> simplifies communication, <br/> making it effortless to stay <br/> updated on campus events, <br/> academic resources, and <br/> news.</p>
+                        <p className="what-we-envision-text">Imagine a platform that allows you post messages and simplify communication. Making it effortless to stay updated on community events, academic resources, and news.</p>
                     </div>
                     <div className="what-we-envision-red-container">
                         <h3 className="what-we-envision-sub-header">Collaboration <br/> Opportunities <img className="what-we-envision-image" src={CollaborationOpportunities} alt="Collaboration Opportunities" width="80" height="80"/></h3>
-                        <p className="what-we-envision-text">Picture a space where you can <br/> easily collaborate with peers on <br/> projects, share insights, and <br/> foster meaningful connections <br/> within the school community.</p>
+                        <p className="what-we-envision-text">Picture a space where you can easily collaborate with peers on projects, share insights, and foster meaningful connections within the community.</p>
                     </div>
-                    <div className="what-we-envision-container">
+                    <div className="what-we-envision-container third">
                         <h3 className="what-we-envision-sub-header">Mentorship & <br/> <span style={{ color: '#ff2e2e' }}>Support</span>
                             <img className="what-we-envision-image" src={PersonalizedExperience} alt="Personalized Experience" width="80" height="80"/>
                         </h3>
-                        <p className="what-we-envision-text-2">Seeking guidance, knowledge, or inspiration? <br/> This app will connect you with ancestors <br/> who can offer valuable insights and support. <br/> Say goodbye to generic advice <br/> and hello to a tailored mentorship experience.</p>
+                        <p className="what-we-envision-text-2">Seeking guidance, knowledge, or inspiration? This app will connect you with ancestors who can offer valuable insights and support. Say goodbye to generic advice and hello to a tailored mentorship experience.</p>
                     </div>
                 </div>
             </div>
             <div className="why-it-matters" id="why-it-matters-section">
                 <h2 className="why-it-matters-header">Why It Matters</h2>
-                <p className="why-it-matters-text">Our vision is driven by a passion to strengthen the bonds within our school community and enhance <br/> the overall experience of being part of this vibrant tech community. We believe that by bringing this <br/> platform to life, we can create a virtual space that mirrors the rich and collaborative environment we <br/> cherish in our physical community.</p>
+                <p className="why-it-matters-text">Our vision is driven by a passion to strengthen the bonds within our school community and enhance the overall experience of being part of this vibrant tech community. We believe that by bringing this platform to life, we can create a virtual space that mirrors the rich and collaborative environment we cherish in our physical community.</p>
             </div>
             <div className="your-role" id="your-role-section">
                 <div className="your-role-left">
                     <h2 className="your-role-header">Your Role</h2>
-                    <p className="your-role-text">We are looking for talented software engineers and <br/> product designers who share our vision and passion <br/> for creating a dynamic and inclusive online hub. <br/> Wherever your specialization, your skills and <br/> expertise are vital to making this vision a reality.</p>
-                    <p className="your-role-register-text">Are you ready to be part of something special? Register your <br/> interest today!</p>
+                    <p className="your-role-text">We are looking for talented software engineers and product designers who share our vision and passion for creating a dynamic and inclusive online hub. Wherever your specialization, your skills and expertise are vital to making this vision a reality.</p>
+                    <p className="your-role-register-text">Are you ready to be part of something special? Register your interest today!</p>
                     <button className="your-role-join-the-team-button" onClick={() => scrollToSection('join-the-team-section')}>Join The Team</button>
                 </div>
                 <div className="your-role-right">
@@ -170,7 +179,7 @@ function LandingPage() {
                 </div>
             </div>
             <div className="join-the-team" id="join-the-team-section">
-                <h2 className="join-the-team-header">Join The Team</h2>
+                <h2 className="join-the-team-header">Join the team</h2>
                 <form className="registration-form" onSubmit={(event) => {
                     handleSubmit(event);
                     }}>
@@ -178,8 +187,10 @@ function LandingPage() {
                         <input placeholder="Full Name                                                     👤" type="text" id="fullName" value={fullName} onChange={handleFullNameChange} autoComplete="new-password"/>
                         <input placeholder="Email Address                                               ✉" type="email" id="email" value={email} onChange={handleEmailChange} autoComplete="new-password"/>
                     </div>
-                    <div>
-                        <PhoneInput country={'ng'} value={phoneNumber} onChange={handlePhoneChange} style={{ marginLeft: '20px', marginTop: '10px', marginBottom: '20px' }}/>
+                    <div className='phone-stack'>
+                        <div className='phone' >
+                            <PhoneInput country={'ng'} value={phoneNumber}  onChange={handlePhoneChange} className="phone-input" />
+                        </div>
                         <select id="stack" value={stack} onChange={handleStackChange}>
                             <option value="">Stack</option>
                             <option value="frontend">Frontend</option>
@@ -213,6 +224,39 @@ function LandingPage() {
                     )}
                 </form>
             </div>
+
+            <footer>
+                <div>
+                    <h3 className='business'> The Team</h3>
+                    <p className='business'>Our Vision
+                    </p>
+                    <p className='business'>Why It Matters</p>
+                    <p className='business'> Your Role</p>
+                </div>
+
+                <div className='child-right'>
+                    <h3 className='elipses'>
+                        ...built by ancestors and natives, for ancestors and natives
+                    </h3>
+                </div>
+                {/* <div>
+                    <h3 className='business'> For Individuals</h3>
+                    <p className='business'>Techpreneurship Program</p>
+                    <p className='business'>Data Analytics Program</p>
+                </div>
+                <div>
+                    <h3 className='business'> About Us</h3>
+                    <p className='business'>Careers</p>
+                    <p className='business'>Reports</p>
+                    <p className='business'> Blog</p>
+                </div>
+                <div>
+                <h3 className='business'> Contact Us</h3>
+                    <p className='business'>312, Herbert Macaulay Way, Sabo Yaba, Lagos.</p>
+                    <p className='business'>+234 906 000 8609</p>
+                    <p className='business'> info@semicolon.africa</p>
+                </div> */}
+            </footer>
         </div>
     );
 }
